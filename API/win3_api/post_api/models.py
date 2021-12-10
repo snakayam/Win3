bc8011b2d7eab3651c043474a3bf04f470946215
ikrfun <takuma130625@keio.jp>
2021-12-10 16:43:00 +0900

    post model改造

--- API/win3_api/post_api/models.py
+++ API/win3_api/post_api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Contents(models.Model):
+    title = models.CharField(max_length=20)
+    thum_img = models.ImageField()
+    contents = models.TextField()
+    made_by = models.ForeignKey(settings.User,)
+    date = models.DateTimeField(auto_now_add=True)
+    last_change = models.DateTimeField(auto_now=True)
+    published = models.BooleanField()
+
--- API/win3_api/post_api/views.py
+++ API/win3_api/post_api/views.py
@@ -1,4 +1,23 @@
+import rest_framework
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework import authentication, permissions
+from rest_framework import generics
+
+class Contents(APIView):
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAdminUser)
+
+    def get(self,request, format = None):
+        pass
+    def post(self,request,format = None):
+        pass
 
 
+class UserList(generics.ListCreateAPIView):
+    queryset = objects.all()
+    serializer_class = ''
+
+class MyPagenations(pageination.PageNumberPagination):
+    page_size = 30
