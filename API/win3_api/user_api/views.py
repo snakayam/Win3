3d25e110f8b71cbed1ab927c7a87c92307c03a42
ikrfun <takuma130625@keio.jp>
2021-12-15 22:53:37 +0900

    runserver clear

--- API/win3_api/user_api/views.py
+++ API/win3_api/user_api/views.py
@@ -40,7 +40,7 @@ class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = serializers.ProfileSerializer
     authentication_classes = (authentication.TokenAuthentication,)
-    permission_classes = (permissions.IsAuthenticated, ProfielePermission)
+    permission_classes = (permissions.IsAuthenticated, ProfilePermission)
 
     def perform_create(self, serializer):
         serializer.save(userPro=self.request.user)
