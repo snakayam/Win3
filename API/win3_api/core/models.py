959a55d18b57d9a6ff3bfbd7ed2586b209621b16
snakayam <yocs0314@gmail.com>
2021-12-15 23:18:27 +0900

    spaceの数直した

--- API/win3_api/core/models.py
+++ API/win3_api/core/models.py
@@ -10,84 +10,84 @@ def upload_path(instance, filename):
 
 class UserManager(BaseUserManager):
     def create_user(self,email,password = None,**extra_fields):
-        
+
         if not email:
             raise ValueError('email is must')
-        
-        
+
+
         user = self.model(email = self.normalize_email(email),**extra_fields)
         user.set_password(password)
         user.save(using = self._db)
-        
+
         return user
-    
+
     def create_superuser(self,email,password):
         user = self.create_user(email,password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using = self._db)
-        
+
         return user
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=50, unique = True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    
+
     objects = UserManager()
     USERNAME_FIELD = 'email'
-    
+
     def __str__(self):
         return self.email
-    
+
 class Profile(models.Model):
     nickName = models.CharField(max_length=20)
-    
+
     user_Pro = models.OneToOneField(
-        settings.AUTH_USER_MODEL, 
+        settings.AUTH_USER_MODEL,
         related_name='userPro',
         on_delete=models.CASCADE
     )
 
     created_on = models.DateTimeField(auto_now_add = True)
     img = models.ImageField(blank = True, null =True, upload_to = upload_path)
-    
+
     def __str__(self):
         return self.nickName
-    
+
 class FriendRequest(models.Model):
     askFrom = models.ForeignKey(
         settings.AUTH_USER_MODEL, related_name = 'askFrom',
         on_delete=models.CASCADE
     )
-    
+
     askTo = models.ForeignKey(
         settings.AUTH_USER_MODEL, related_name = 'askTo',
         on_delete=models.CASCADE
     )
-    
+
     approved = models.BooleanField(default=False)
-    
+
     class Meta:
         unique_together = (('askFrom','askTo'),)
-        
+
     def __str__(self):
         return str(self.askFrom) + '----->'+str(self.askTo)
-    
+
 class Message(models.Model):
     message = models.CharField(max_length=140)
     sender = models.ForeignKey(
         settings.AUTH_USER_MODEL, related_name = 'sender',
         on_delete=models.CASCADE
     )
-    
+
     receiver = models.ForeignKey(
         settings.AUTH_USER_MODEL, related_name = 'receiver',
         on_delete=models.CASCADE
     )
-    
+
     def __str__(self):
         return self.sender
-    
+
 class Contents(models.Model):
     title = models.CharField(max_length=20)
     thum_img = models.ImageField()
@@ -101,10 +101,10 @@ class Contents(models.Model):
 class Favo(models.Model):
     date = models.DateTimeField(auto_now_add = True)
     favoFrom = models.ForeignKey(
-	    settings.AUTH_USER_MODEL, related_name = 'favoFrom',
-	    on_delete=models.CASCADE
+        settings.AUTH_USER_MODEL, related_name = 'favoFrom',
+        on_delete=models.CASCADE
 	)
     favoTo =  models.ForeignKey(
-	    Contents, related_name = 'favoFrom',
-	    on_delete=models.CASCADE
+        Contents, related_name = 'favoFrom',
+        on_delete=models.CASCADE
 	)
