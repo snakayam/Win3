865ee2290912a0da6ff44d1de9a3942aaee47e67
ikrfun <takuma130625@keio.jp>
2022-01-30 12:01:08 +0900

    デプロイ用にAPI関係以外の要素を削除した

--- API/win3_api/core/models.py
+++ API/win3_api/core/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
 from django.conf import settings
+from django.utils import timezone
 
 
 def upload_path(instance, filename):
@@ -11,12 +12,15 @@ def upload_path(instance, filename):
 
 class UserManager(BaseUserManager):
     def create_user(self,email,password = None,**extra_fields):
-
+        now = timezone.now()
         if not email:
             raise ValueError('email is must')
 
 
-        user = self.model(email = self.normalize_email(email),**extra_fields)
+        user = self.model(
+            email = self.normalize_email(email),
+            **extra_fields
+        )
         user.set_password(password)
         user.save(using = self._db)
 
